8195db2309e79e7a3f5244ad4fc5da550c586d0b
Austin Peterson <austin@akpwebdesign.com>
2017-07-16 18:14:46 -0400

    feat: Discord connector

---
+++ index.js
@@ -0,0 +1,94 @@
+const admin = require('firebase-admin');
+const serviceAccount = require("./serviceAccount.json");
+const Eris = require('eris');
+const Promise = require('bluebird');
+let cid, token, name, bot;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://basedakp48.firebaseio.com"
+});
+
+const rootRef = admin.database().ref();
+
+try {
+  cid = require('./cid.json');
+} catch (e) {
+  let fs = require('fs');
+  let clientRef = rootRef.child('config/clients').push();
+  cid = clientRef.key;
+  fs.writeFileSync('./cid.json', JSON.stringify(cid), {encoding: 'UTF-8'});
+}
+
+rootRef.child(`config/clients/${cid}`).on('value', (d) => {
+  let config = d.val();
+
+  if(bot) {
+    if(config.token !== token) {
+      // we need to disconnect the bot and connect with our new token.
+    }
+
+    if(config.name !== name) {
+      name = config.name;
+    }
+  } else { // no bot yet
+    token = config.token;
+    name = config.name;
+    bot = new Eris(token, {
+      // options will go here later.
+    });
+
+    initializeBot();
+  }
+});
+
+function initializeBot() {
+  bot.on('ready', () => {
+    console.log('connected to Discord');
+    bot.editStatus("online", {
+      name: 'with code.',
+      type: 0,
+      url: 'https://akp48.akpwebdesign.com/'
+    });
+  });
+
+  bot.on('messageCreate', (msg) => {
+    handleMessage(msg);
+  });
+
+  rootRef.child(`clients/${cid}`).on('child_added', (d) => {
+    let msg = d.val();
+    if(msg.msgType.toLowerCase() === 'chatmessage') {
+      return bot.sendChannelTyping(msg.channel).then(() => {
+        return Promise.delay(750).then(() => {
+          if(msg.extra_client_info && msg.extra_client_info.discord_embed) {
+            return bot.createMessage(msg.channel, {embed: msg.extra_client_info.discord_embed});
+          }
+          return bot.createMessage(msg.channel, msg.text);
+        });
+      }).then(() => {
+        return d.ref.remove();
+      });
+    }
+  });
+
+  bot.connect();
+}
+
+function handleMessage(msg) {
+  if(msg.author.id == bot.user.id) {
+    console.log(`<=== ${msg.author.username}#${msg.author.discriminator} | ${msg.channel.guild.name}/${msg.channel.name}: ${msg.content}`);
+    return;
+  }
+  let BasedAKP48Msg = {
+    cid: cid,
+    uid: msg.author.id,
+    text: msg.content,
+    channel: msg.channel.id,
+    msgType: 'chatMessage',
+    timeReceived: msg.timestamp
+  };
+
+  rootRef.child('incomingMessages').push().set(BasedAKP48Msg);
+  console.log(`===> ${msg.author.username}#${msg.author.discriminator} | ${msg.channel.guild.name}/${msg.channel.name}: ${msg.content}`);
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# basedakp48-plugin-discord-connector
+
+> BasedAKP48 Discord Connector
+
+
+## Install
+
+```
+$ npm install basedakp48-plugin-discord-connector
+```
+
+## License
+
+MIT © [Austin Peterson](https://akp48.akpmakes.tech)
